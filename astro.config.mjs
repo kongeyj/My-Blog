08a9c1138e8e01e2457b738bb1fcf60805d65aff
Your Name <you@example.com>
2024-08-13 18:09:39 +0800

    修改了README，移除了astrojs/db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import db from "@astrojs/db";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact(), db(), tailwind()],
-  output: "server"
+  integrations: [preact(), tailwind()],
+  // output: "server",
 });
